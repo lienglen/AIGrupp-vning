5a3f2a5553b9e007457a689e42024cf2f0d13778
Markus Klintstedt <markus.klint@gmail.com>
2026-01-19 16:06:36 +0100

    Fix 4

--- ViewModels/MessagesViewModel.cs
+++ ViewModels/MessagesViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using AIGruppÖvning.Command;
+using AIGruppÖvning.Enums;
 using AIGruppÖvning.Models;
 using OpenAI.Chat;
 
