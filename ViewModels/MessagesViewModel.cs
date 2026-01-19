0b2ab4b79c580efa0fee39712edb8a674993e748
Smockelito <amandavikberg90@gmail.com>
2026-01-19 14:03:29 +0100

    Changed in MessageViewModel

--- ViewModels/MessagesViewModel.cs
+++ ViewModels/MessagesViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using AIGruppÖvning.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AIGruppÖvning.ViewModels
 {
-    internal class MessagesViewModel
+    public class MessagesViewModel : BaseViewModel
     {
-    }
+		private ObservableCollection<Message> messages;
+
+		public ObservableCollection<Message> Messages
+		{
+			get { return messages; }
+			set { messages = value; }
+		}
+
+	}
 }
