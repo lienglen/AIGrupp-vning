381d817b2fdd37af63e33cf0935a1a763e8a2feb
Smockelito <amandavikberg90@gmail.com>
2026-01-19 15:11:00 +0100

    added constructor to MessageViewModel

--- ViewModels/MessagesViewModel.cs
+++ ViewModels/MessagesViewModel.cs
@@ -1,16 +1,18 @@
-﻿using AIGruppÖvning.Models;
+﻿using AIGruppÖvning.Command;
+using AIGruppÖvning.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace AIGruppÖvning.ViewModels
 {
     public class MessagesViewModel : BaseViewModel
     {
-		private ObservableCollection<Message> messages;
+		private ObservableCollection<Message> messages = new();
 
 		public ObservableCollection<Message> Messages
 		{
@@ -20,16 +22,26 @@ namespace AIGruppÖvning.ViewModels
 			}
 		}
 
-		public void AddMessage(Message message)
+        public DelegateCommand AddCommand { get; private set; }
+
+        public MessagesViewModel()
+        {
+            messages.Add(new Message() {MessageId = 1, SenderId = 1, Content = "Hej", Timestamp = DateTime.Now});
+
+			AddCommand = new DelegateCommand(AddMessage);
+        }
+
+        public void AddMessage(object? parameter)
 		{
+			var message = new Message();
 			messages.Add(message);
-			RaisePropertyChanged(nameof(Messages));
+			RaisePropertyChanged();
         }
 
 		public void ClearChat()
 		{
 			messages.Clear();
-			RaisePropertyChanged(nameof(Messages));
+			RaisePropertyChanged();
         }
 
         public async Task LoadAsync()
