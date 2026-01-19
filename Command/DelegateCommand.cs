ffc1462e65f3fb2b2c2d59dfde40c59bf428f106
Smockelito <amandavikberg90@gmail.com>
2026-01-19 14:25:13 +0100

    Added constructor

--- Command/DelegateCommand.cs
+++ Command/DelegateCommand.cs
@@ -9,7 +9,16 @@ namespace AIGruppÖvning.Command
 {
     public class DelegateCommand : ICommand
     {
+        private readonly Action<object?> execute;
+        private readonly Func<object?, bool>? canExecute;
+
         public event EventHandler? CanExecuteChanged;
+
+        public DelegateCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
 
         public bool CanExecute(object? parameter)
         {
